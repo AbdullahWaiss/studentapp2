08d036a402cb9d3572ee0bcb65c2eb4b404eb7d5
AbdullahWaiss <abdullahwaiss@icloud.com>
2023-09-04 20:18:31 +0200

    first commit

---
+++ webapp/controller/DetailObjectNotFound.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+    "./BaseController"
+], function (BaseController) {
+    "use strict";
+
+    return BaseController.extend("be.ap.sap.studentapp2.controller.DetailObjectNotFound", {});
+});
---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,8 @@
+/* global QUnit */
+
+sap.ui.require([
+	"be/ap/sap/studentapp2/test/integration/AllJourneys"
+], function() {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
